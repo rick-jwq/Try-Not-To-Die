3f67557ca29e173abfb5cacd7a3e346e01ea7a77
KaihengZ <kaihengz@usc.edu>
2021-02-03 22:41:47 -0800

    adding food and water gameobjects
    1

---
+++ Assets/Scripts/Water.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
