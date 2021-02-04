5219bb432e544c675cc0e8f1d1ac7a3f4a4a0c0e
Yiming Wang <zdwym1997@gmail.com>
2021-02-03 17:40:41 -0800

    Basic Moving Ground

---
+++ Assets/SpawnFloor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFloor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Floor;
+    public bool destroy = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(destroy)
+        {
+            Destroy(transform.parent.gameObject);
+        }
+        else
+        {
+            float new_x = transform.position.x + 20f;
+            Instantiate(Floor, new Vector3(new_x, transform.position.y, transform.position.z), Quaternion.identity);
+        }
+    }
+}
