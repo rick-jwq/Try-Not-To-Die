79db414a16a42e543a6b6011eaeba76cffa6a80b
Yiming Wang <zdwym1997@gmail.com>
2021-02-07 14:04:13 -0800

    add enemy stuff

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float knockOutForce = 20f;
+    private float hp = 100f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(hp <= 0f)
+        {
+            rb.AddForce(transform.forward * knockOutForce);
+            Destroy(gameObject, 2f);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        hp -= 10f;
+    }
+
+    public float getHP()
+    {
+        return hp;
+    }
+}
---
+++ Assets/Scripts/EnemyHPBar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHPBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform targetEnemy;
+
+    private RectTransform HPValueBar;
+    private Enemy enemyScript;
+
+    private void Start()
+    {
+        HPValueBar = GetComponentsInChildren<RectTransform>()[1];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject enemy = GameObject.FindGameObjectWithTag("Enemy");
+        if (enemy)
+        {
+            gameObject.SetActive(true);
+            Enemy enemyScript = enemy.GetComponent<Enemy>();
+            Vector3 headPosition = Camera.main.WorldToScreenPoint(enemy.transform.GetChild(0).position);
+            transform.position = headPosition;
+
+            float enemyHP = enemyScript.getHP();
+            HPValueBar.sizeDelta = new Vector2(enemyHP * 2, 40);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
