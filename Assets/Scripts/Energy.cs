66001d6b0a59a3343374a51c5b13e069b27e32ea
Yiming Wang <zdwym1997@gmail.com>
2021-02-16 16:53:46 -0800

    Ready for next sprint

--- Assets/Scripts/Energy.cs
+++ Assets/Scripts/Energy.cs
@@ -2,36 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Water : MonoBehaviour
+public class Energy : MonoBehaviour
 {
-    public float speed = 5f;
-    // Start is called before the first frame update
 
     private CharacterControll cc;
+    private float spawnTime;
+    // Start is called before the first frame update
     void Awake()
     {
-        transform.position = new Vector3(Random.Range(15, 30), 1, Random.Range(2, 7));
+        transform.position = new Vector3(Random.Range(15, 30),1, Random.Range(2, 7));
     }
     void Start()
     {
         cc = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterControll>();
+        spawnTime = Time.time;
     }
-
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(-1, 0, 0) * Time.deltaTime * speed);
-        if (transform.position.x < -8)
+        if (Time.time - spawnTime >= 10f)
         {
             Destroy(gameObject);
         }
     }
     void OnMouseOver()
     {
-        if(Input.GetMouseButtonDown(1))
+        if(Input.GetMouseButtonDown(0))
         {
             Destroy(gameObject);
-            cc.ChangeThirst(20);
+
+            //Change to changeYinYang
+            cc.ChangeHunger(10);
         }
     }
 }
---
+++ Assets/Scripts/EnergyGeneration.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyGeneration : MonoBehaviour
+{
+    public float ChanceToSpawn = 0.4f;
+    public GameObject yinPrefab;
+    public GameObject yangPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("EnergySpawn",2f,5f);
+    }
+
+    void EnergySpawn()
+    {
+        if(Random.Range(0f,1f) > 1f - ChanceToSpawn)
+        {
+            float x = Random.Range(15, 30);
+            float y = 1;
+            float z = Random.Range(2, 7);
+
+            if (Random.Range(0f, 1f) > 0.5f)
+            {
+                GameObject energy = Instantiate(yinPrefab, new Vector3(x, y, z), Quaternion.identity);
+                Destroy(energy, 10f);
+            }
+            else
+            {
+                GameObject energy = Instantiate(yangPrefab, new Vector3(x, y, z), Quaternion.identity);
+                Destroy(energy, 10f);
+            }
+        }
+    }
+}
