76ea490a30662044767f87fed2130a85bf958a2f
Yiming Wang <zdwym1997@gmail.com>
2021-03-09 12:26:54 -0800

    Add UI for game start, upgrade, tutorial

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void LoadLevel(int level_number)
+    {
+        SceneManager.LoadScene(level_number);
+    }
+}
